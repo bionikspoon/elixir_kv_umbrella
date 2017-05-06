74bab6f828052aa37694812ffd272ccacefc1eb3
Manu Phatak <manu@thinkcerca.com>
2017-05-06 01:31:00 -0500

    Use :ets

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -3,48 +3,52 @@ defmodule KV.Registry do
 
   ## Client API
 
-  @doc "Starts the registry"
-  def start_link(name), do: GenServer.start_link(__MODULE__, :ok, name: name)
+  @doc "Starts the registry with given `name`"
+  def start_link(name), do: GenServer.start_link(__MODULE__, name, name: name)
 
   @doc """
   Looks up bucket pid for `name` stored in `server`.
 
   Returns `{:ok, pid}` if the bucket exists, `:error` otherwise
   """
-  def lookup(server, name), do: GenServer.call(server, {:lookup, name})
+  def lookup(server, name) when is_atom(server)   do
+    case :ets.lookup(server, name) do
+      [{^name, pid}] -> {:ok, pid}
+      [] -> :error
+    end
+   end
 
   @doc "Ensure there is a bucket `name` in `server`"
-  def create(server, name), do: GenServer.cast(server, {:create, name})
+  def create(server, name), do: GenServer.call(server, {:create, name})
 
   @doc "Stops the registry"
   def stop(server), do: GenServer.stop(server)
 
   ## Server Callbacks
-  def init(:ok) do
-    names = %{}
+  def init(table) do
+    names = :ets.new(table, [:named_table, read_concurrency: true])
     refs = %{}
 
     {:ok, {names, refs}}
   end
 
-  def handle_call({:lookup, name}, _from, {names, _} = state), do: {:reply, Map.fetch(names, name), state}
 
-  def handle_cast({:create, name}, {names, refs}) do
-    if Map.has_key?(names, name)  do
-      {:noreply, {names, refs}}
-    else
-      {:ok, pid} = KV.Bucket.Supervisor.start_bucket
-      ref = Process.monitor(pid)
-      refs = Map.put(refs, ref, name)
-      names = Map.put(names, name, pid)
-
-      {:noreply, {names, refs}}
+  def handle_call({:create, name}, _from, {names, refs}) do
+    case lookup(names, name) do
+      {:ok, pid} ->
+        {:reply, pid, {names, refs}}
+      :error ->
+        {:ok, pid} = KV.Bucket.Supervisor.start_bucket
+        ref = Process.monitor(pid)
+        refs = Map.put(refs, ref, name)
+        :ets.insert(names, {name, pid})
+        {:reply, pid, {names, refs}}
     end
   end
 
   def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
     {name, refs} = Map.pop(refs, ref)
-    names = Map.delete(names, name)
+    :ets.delete(names,name)
     {:noreply, {names, refs}}
   end
 
